13eab60cd08be7ef8b58c65cc5fd72315ba1216e
Elensh <dodydodey2010@yahoo.com>
2021-02-26 11:13:23 +0200

    admin LTE && laravelLocalization

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.dashboard.app')
+
+@section('content')
+
+    <div class="content-wrapper">
+
+        <section class="content-header">
+
+            <h1>This is the Dashboard</h1>
+
+            <ol class="breadcrumb">
+                <li class="active"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</li>
+            </ol>
+        </section>
+    </div>
+
+@endsection
